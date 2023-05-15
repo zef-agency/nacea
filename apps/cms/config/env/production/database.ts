ca6f6043d5e46f523efe5d32d1bc3b55c967a0a2
foucauld <foucauldbeaumont@yahoo.fr>
2023-05-15 23:41:30 +0200

    ssl true

--- apps/cms/config/env/production/database.ts
+++ apps/cms/config/env/production/database.ts
@@ -13,7 +13,9 @@ module.exports = ({ env }) => {
         user,
         password,
       },
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: true,
+      },
       debug: false,
     },
   };
