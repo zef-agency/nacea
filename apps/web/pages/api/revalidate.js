fbb5a49a2845eace01c35e1ccaaee651b022232a
foucauld <foucauldbeaumont@yahoo.fr>
2023-05-23 18:09:19 +0200

    delete revalidate

--- apps/web/pages/api/revalidate.js
+++ apps/web/pages/api/revalidate.js
@@ -6,14 +6,14 @@ export default async function handler(req, res) {
   try {
     await res.revalidate("/");
     console.log("home ok");
-    await res.revalidate("/event");
+    /*     await res.revalidate("/event");
     console.log("event ok");
     await res.revalidate("/contact");
     console.log("contact ok");
     await res.revalidate("/concept");
     console.log("concept ok");
     await res.revalidate("/mentions-legales");
-    console.log("ml ok");
+    console.log("ml ok"); */
 
     return res.json({ revalidated: true });
   } catch (err) {
