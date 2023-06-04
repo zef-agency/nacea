ed63b93b0f0ed874d851e4507c79301b5a0057c1
foucauld <foucauldbeaumont@yahoo.fr>
2023-06-04 19:42:26 +0200

    add render yaml

--- apps/cms/config/env/production/server.ts
+++ apps/cms/config/env/production/server.ts
@@ -1,8 +1,13 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({ env }) => ({
+/* export default ({ env }) => ({
   proxy: true,
   url: env("APP_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
+}); */
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ env }) => ({
+  url: env("RENDER_EXTERNAL_URL"),
 });
