860b2d5cd967e0edfd2d91a84449687ae0dd78e9
foucauld <foucauldbeaumont@yahoo.fr>
2023-06-04 18:17:03 +0200

    add changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,12 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui", "utils"],
   images: {
-    domains: ["localhost", "res.cloudinary.com", "scontent.cdninstagram.com"],
+    domains: [
+      "localhost",
+      "res.cloudinary.com",
+      "scontent.cdninstagram.com",
+      "scontent-cdg4-3.cdninstagram.com",
+      "scontent-cdg4-2.cdninstagram.com",
+    ],
   },
 };
