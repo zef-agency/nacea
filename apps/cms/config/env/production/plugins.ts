9e44bdc182c7c652aad355cb793f8d84fd053a41
foucauld <foucauldbeaumont@yahoo.fr>
2023-06-05 09:04:16 +0200

    remove cloudinary in devmode

--- apps/cms/config/env/production/plugins.ts
+++ apps/cms/config/env/production/plugins.ts
@@ -12,7 +12,9 @@ module.exports = ({ env }) => ({
         api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
-        upload: {},
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER", "NACEA"),
+        },
         delete: {},
       },
     },
