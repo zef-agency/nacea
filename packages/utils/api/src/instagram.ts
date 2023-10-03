89b19894fe001235c295c57d195f8e0f6ffac56f
foucauld <foucauldbeaumont@yahoo.fr>
2023-10-03 14:29:34 +0200

    switch insta logic and add refresh token

--- packages/utils/api/src/instagram.ts
+++ packages/utils/api/src/instagram.ts
@@ -1,14 +1,14 @@
 import useSWR from "swr";
-import { fetcher } from "utils";
+import { fetcher, getUrl } from "utils";
 
 // ----- GET INSTAGRAM POSTS -----
 export function GetInstagramPosts() {
-  const url = `https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink,caption&limit=6&access_token=${process.env.NEXT_PUBLIC_IG_TOKEN}`;
-
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR([getUrl("/api/instagram-posts")], ([url]) =>
+    fetcher(url)
+  );
 
   return {
-    posts: data?.success?.data,
+    posts: data?.success,
     isPostsLoading: !error && !data,
     isPostsError: data?.error,
   };
